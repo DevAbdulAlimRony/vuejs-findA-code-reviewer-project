4bd5f6067919974277596a3e69aca519d42f636e
Md. Abdul Alim <92217313+AbdulAlimRony@users.noreply.github.com>
2023-09-29 22:21:17 +0600

    components design

---
+++ src/components/ui/BaseButton.vue
@@ -0,0 +1,32 @@
+<script>
+    export default{
+        props: {
+            mode: {
+                type: String,
+                required: false,
+                default: null
+            },
+            link: {
+                type: Boolean,
+                required: false,
+                default: false
+            },
+
+            to: {
+                type: String,
+                required: false,
+                default: '/'
+            }
+        }
+    }
+</script>
+
+<template>
+    <button v-if="!link" :class="mode">
+        <slot></slot>
+    </button>
+
+    <router-link v-else :to="to" :class="mode">
+        <slot></slot>
+    </router-link>
+</template>
--- src/router.js
+++ src/router.js
@@ -1,20 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachRegistration from './pages/coaches/CoachRegister.vue';
+
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestRecieved from './pages/requests/RequestRecieved.vue';
+
+import NotFound from './pages/NotFound.vue';
+
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', redirect: '/coaches' },
-        { path: '/coaches', component: null },
+        { path: '/coaches', component: CoachesList },
         {
             path: '/coaches/:id',
-            component: null,
+            component: CoachDetail,
             children: [
-                {path: 'contact', component: null}
+                {path: 'contact', component: ContactCoach}
             ]
         },
-        { path: '/register', component: null },
-        { path: '/request', component: null },
-        { path: '/:notFound(.*)', component: null }
+        { path: '/register', component: CoachRegistration },
+        { path: '/request', component: RequestRecieved },
+        { path: '/:notFound(.*)', component: NotFound }
     ]
 });
 
